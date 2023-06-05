0a3e1575530b1425a8ed4c27626860a07aa8f095
Akas_Datta <012akasdatta@gmail.com>
2023-06-05 17:32:21 +0600

    added usercollection

--- index.js
+++ index.js
@@ -26,76 +26,95 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
 
+    const usersCollection = client.db("bistroDb").collection("users");
     const menuCollection = client.db("bistroDb").collection("menu");
     const reviewCollection = client.db("bistroDb").collection("reviews");
     const cartCollection = client.db("bistroDb").collection("carts");
 
+    // users related apis
+    app.get('/users', async(req, res) =>{
+      const result = await usersCollection.find().toArray();
+      res.send(result);
+    })
+    app.post('/users', async (req, res) => {
+      const user = req.body;
+      const query = {email: user.email}
+      const existingUser = await usersCollection.findOne(query);
+
+      if(existingUser){
+        return res.send({message: 'User already exists'})
+      }
+      const result = await usersCollection.insertOne(user);
+      res.send(result);
+    })
+
+    // menu related apis
     app.get('/menu', async (req, res) => {
       const result = await menuCollection.find().toArray();
       res.send(result);
     })
 
+    // review related apis
     app.get('/reviews', async (req, res) => {
       const result = await reviewCollection.find().toArray();
       res.send(result);
     })
 
 
-       // cart collection apis
-       app.get('/carts', async (req, res) => {
-        const email = req.query.email;
-  
-        if (!email) {
-          res.send([]);
-        }
-        const query = { email: email };
-        const result = await cartCollection.find(query).toArray();
-        res.send(result);
-      });
-  
-      app.post('/carts', async (req, res) => {
-        const item = req.body;
-        console.log(item);
-        const result = await cartCollection.insertOne(item);
-        res.send(result);
-      })
+    // cart collection apis
+    app.get('/carts', async (req, res) => {
+      const email = req.query.email;
 
-      app.delete('/carts/:id', async(req, res) =>{
-        const id = req.params.id;
-        const query = {_id: new ObjectId(id)};
-        const result = await cartCollection.deleteOne(query);
-        res.send*(result);
-      })
+      if (!email) {
+        res.send([]);
+      }
+      const query = { email: email };
+      const result = await cartCollection.find(query).toArray();
+      res.send(result);
+    });
 
-      // Send a ping to confirm a successful connection
-      await client.db("admin").command({ ping: 1 });
-      console.log("Pinged your deployment. You successfully connected to MongoDB!");
-    } finally {
-      // Ensures that the client will close when you finish/error
-      // await client.close();
-    }
-    }
-    run().catch(console.dir);
-
-
-    app.get('/', (req, res) => {
-    res.send('boss is sitting')
+    app.post('/carts', async (req, res) => {
+      const item = req.body;
+      const result = await cartCollection.insertOne(item);
+      res.send(result);
     })
 
-    app.listen(port, () => {
-    console.log(`Bistro boss is sitting on port ${port}`);
+    app.delete('/carts/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await cartCollection.deleteOne(query);
+      res.send(result);
     })
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+
+app.get('/', (req, res) => {
+  res.send('boss is sitting')
+})
+
+app.listen(port, () => {
+  console.log(`Bistro boss is sitting on port ${port}`);
+})
 
 
 /**
- * ----------------------
- * NAMING CONVENTION
- * ---------------------
- * users: userCollection
+ * --------------------------------
+ *      NAMING CONVENTION
+ * --------------------------------
+ * users : userCollection
  * app.get('/users')
  * app.get('/users/:id')
- * app.post('/users/')
+ * app.post('/users')
  * app.patch('/users/:id')
  * app.put('/users/:id')
  * app.delete('/users/:id')
- */
+*/
