f195220b62db36b141a5762a6d3d0896a4f5c42e
Akas_Datta <012akasdatta@gmail.com>
2023-06-06 17:16:39 +0600

    Make user api secure and verify Admin

--- index.js
+++ index.js
@@ -58,8 +58,28 @@ async function run() {
       res.send({token})
     })
 
+    // warning: use verifyJWT before using verifyAdmin
+    const verifyAdmin = async(req, res, next) =>{
+      const email = req.decoded.email;
+      const query = {email: email}
+      const user = await usersCollection.findOne(query);
+      if(user?.role !== 'admin'){
+        return res.status(403).send({error: true, message: 'forbidden message'});
+      }
+      next();
+    }
+
+
+    /**
+     * 0. Do not show secure links to those who should not use the links
+     * 1. use jwt token: verifyJWT
+     * 2. use verifyAdmin middleware
+     */
+
+
+
     // users related apis
-    app.get('/users', async(req, res) =>{
+    app.get('/users', verifyJWT, verifyAdmin, async(req, res) =>{
       const result = await usersCollection.find().toArray();
       res.send(result);
     })
@@ -78,7 +98,7 @@ async function run() {
     app.get('users/admin/:email', verifyJWT, async(req, res) => {
       const email = req.params.email;
 
-      
+
       const query = {email: email}
       const user = await usersCollection.findOne(query);
       const result = {admin: user?.role === 'admin'}
